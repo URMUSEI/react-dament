6868f73f4ed53f5eef91eb9802cd01feae207b2b
URMUSEI <101331089+URMUSEI@users.noreply.github.com>
2022-08-04 14:21:10 +0900

    bb

---
+++ src/Coms/Customer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Customer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>NO</h2>
+                <p>mail</p>
+                <p>pwd</p>
+            </div>
+        )
+    }
+}
+
+export default Customer;
