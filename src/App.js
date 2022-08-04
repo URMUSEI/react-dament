6b6a2880836a183a4413d74b6d8f9cdaebfd3d8b
URMUSEI <101331089+URMUSEI@users.noreply.github.com>
2022-08-04 16:51:57 +0900

    re

--- src/App.js
+++ src/App.js
@@ -2,41 +2,38 @@ import logo from './logo.svg';
 import Customer from './Coms/Customer'
 import React, { Component } from 'react';
 import './App.css';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
 
 const customers = [{
-  'id':'1',
-  'no':'1',
-  'mail':'2',
-  'pwd':'3'
+  'id':1,
+  'no':'2',
+  'mail':'3',
+  'pwd':'4'
 },
 {
-  'id':'2',
-  'no':'1',
-  'mail':'2',
-  'pwd':'3'
+  'id':2,
+  'no':'2',
+  'mail':'3',
+  'pwd':'4'
 },
 {
-  'id':'3',
-  'no':'1',
-  'mail':'2',
-  'pwd':'3'
+  'id':3,
+  'no':'2',
+  'mail':'3',
+  'pwd':'4'
 }]
 
 function App() {
-  return (
+  return ( 
     <div>
-      <Table>
-        <TableBody>
-     { customers.map(c => { return ( <Customer key={c.id} id={c.id} no={c.no} mail={c.mail} pwd={c.pwd} /> ) }) }
-     </TableBody>
-     </Table>
+     {
+      customers.map(c => {
+        return(
+          <Customer key={c.id} id={c.id} no={c.no} mail={c.mail} pwd={c.pwd} />
+        )
+      })
+     }
     </div>
-  );
-}
 
+  )
+}
 export default App;
