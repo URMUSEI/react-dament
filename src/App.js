9f0b755caba50f49d29087df1b24777d3df2f49d
URMUSEI <101331089+URMUSEI@users.noreply.github.com>
2022-08-09 04:09:20 +0900

    scroll bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import Customer from './Coms/Customer';
 import './App.css';
+import { Paper } from '@mui/material';
 import { Table } from '@mui/material';
 import { TableHead } from '@mui/material';
 import { TableBody } from '@mui/material';
 import { TableRow } from '@mui/material';
 import { TableCell } from '@mui/material';
+import { alpha } from '@mui/material/styles';
+// import { makeStyles } from '@mui/styles';
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+        margiTop: theme.spacing.unit*3,
+        overflowX: "auto"
+    },
+    table: {
+        minWidth: 1080
+    }
+})
 
 const customers = [{
     'id':1,
@@ -32,12 +46,26 @@ const customers = [{
     'job': '대학생'
 }]
 
-function App () {
+class App extends Component {
+    render() {
+        const { classes } = this.props;
         return(
-            <div>
+            <Paper className={classes.root}>
+            <Table className={classes.table}>
+                <TableHead>
+                    <TableCell>번호</TableCell>
+                    <TableCell>이름</TableCell>
+                    <TableCell>생년월일</TableCell>
+                    <TableCell>성별</TableCell>
+                    <TableCell>직업</TableCell>
+                </TableHead>
+                <TableBody>
                 {customers.map(c=>{return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
-            </div>
+                </TableBody>
+            </Table>
+            </Paper>
         );
+        }
     }
 
-export default App;
+export default alpha(styles)(App);
--- src/Coms/Customer.js
+++ src/Coms/Customer.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { TableRow } from '@mui/material';
 import { TableCell } from '@mui/material';
-import { Table }
 
 class Customer extends React.Component {
     render() {
         return (
             <TableRow>
-                <TableCell>{this.props.id}</TableCell><TableCell><img src={this.props.image} alt='profile' /></TableCell><TableCell>{this.props.name}</TableCell><TableCell>{this.props.birthday}</TableCell><TableCell>{this.props.gender}</TableCell><TableCell>{this.props.job}</TableCell>
+                <TableCell>{this.props.id}</TableCell><TableCell>{this.props.name}</TableCell><TableCell>{this.props.birthday}</TableCell><TableCell>{this.props.gender}</TableCell><TableCell>{this.props.job}</TableCell>
             </TableRow>
         )
     }
@@ -17,7 +16,6 @@ class CustomerProfile extends React.Component {
     render() {
         return (
             <div>
-                <img src={this.props.image} alt="profile"/>
                 <h2>{this.props.name}({this.props.id})</h2>
             </div>
         )
