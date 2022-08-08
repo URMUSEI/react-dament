075b6a5522266c6003b6e041ae9592bfcd3ec8e9
URMUSEI <101331089+URMUSEI@users.noreply.github.com>
2022-08-08 18:07:18 +0900

    re

--- src/Coms/Customer.js
+++ src/Coms/Customer.js
@@ -4,32 +4,11 @@ class Customer extends React.Component {
     render() {
         return (
             <div>
-                <CustomerProfile mail={this.props.mail} id={this.props.id} />
-                <CustomerInfo no={this.props.no} mail={this.props.mail} pwd={this.props.pwd} />
+                <h2>hello</h2><p>li</p>
             </div>
         )
     }
 }
 
-class CustomerProfile extends React.Component{
-    render() {
-        return (
-            <div>
-                <h2>{this.props.mail}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
-class CustomerInfo extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>{this.props.no}</p>
-                <p>{this.props.mail}</p>
-                <p>{this.props.pwd}</p>
-            </div>
-        )
-    }
-}
 
 export default Customer;
